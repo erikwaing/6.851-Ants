779ac3ea3539a1a6b5a412ba73a82f9bbaff8654
Erik Waingarten <erikwaing@gmail.com>
2014-10-19 01:24:48 -0400

    initial commit

---
+++ code/algorithms/FKLS.py
@@ -0,0 +1,18 @@
+import random
+
+class FKLS1:
+    
+    def __init__(location):
+        self.location = location
+        self.j = 1
+        self.i = 1
+        self.u = None
+
+    def getLocation():
+        return self.location
+
+    
+    def act(numSteps):
+        while True:
+            while self.i <= self.j:
+                
---
+++ code/algorithms/ant.py
@@ -0,0 +1,9 @@
+class ant:
+    
+    def act():
+        raise NotImplementedError('Someone should have implemented this!')
+
+    def getLocaion():
+        raise NotImplementedError('Someone should have implemented this!')
+
+    
