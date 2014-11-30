be651dc0d78abfd4bb337ed79942ab9be7b3ef3a
Nicolas Rakover <nrakover@gmail.com>
2014-11-30 15:18:50 -0500

    added Lines algo uniform in everything and derandomized the algo uniform in D

--- code/algorithms/Lines.py
+++ code/algorithms/Lines.py
@@ -82,8 +82,9 @@ class LinesUniformInD(LinesNonUniform):
 		self.n = n
 		self.K = K
 		self.i = 0
-		self.step = 'start'
-		self.simulation = None
+		self.j = 1
+		self.step = 'simulate'
+		self.simulation = LinesNonUniform(self.source, 1)
 
 
 	def getLocation(self):
@@ -92,38 +93,91 @@ class LinesUniformInD(LinesNonUniform):
 		return self.simulation.getLocation()
 
 	def act(self):
-		if self.step == 'start':
-			self.start()
+		if self.step == 'advance':
+			self.advance()
 		elif self.step == 'simulate':
 			self.simulate()
 
-	def start(self):
-		self.simulation = LinesNonUniform(self.source, math.pow(2 , 2*self.i))
-		self.trySearch()
-
-	def trySearch(self):
+	def advance(self):
 		p = self.K + max( self.i - math.floor( math.log(self.n , 2) ) , 0 )
-		if random.random() > (1.0 / p):
-			self.step = 'simulate'
-			self.simulate()
-		else:
+		max_j = int(2**p)
+		if self.j == max_j:
 			self.i += 1
-			self.step = 'start'
+			self.j = 1
+			LinesNonUniform(self.source, 2**self.i)
+		else:
+			self.j += 1
+
+		self.step = 'simulate'
+		self.simulate()
+
+	# def start(self):
+	# 	self.simulation = LinesNonUniform(self.source, math.pow(2 , self.i))
+	# 	self.trySearch()
+
+	# def trySearch(self):
+	# 	p = self.K + max( self.i - math.floor( math.log(self.n , 2) ) , 0 )
+	# 	if random.random() > (1.0 / (2**p)):
+	# 		self.step = 'simulate'
+	# 		self.simulate()
+	# 	else:
+	# 		self.i += 1
+	# 		self.step = 'start'
 
 	def simulate(self):
 		if self.simulation.step == 'origin':
 			self.simulation.act()
-			self.trySearch()
+			self.step = 'advance'
 		else:
 			self.simulation.act()
 
 
 class LinesUniformInAll(LinesUniformInD):
 
-	def __init__(self, location, f, K):
-		LinesUniformInD.__init__(self, location, 0, K)
-		self.l = 0
-		self.f = f
+	def __init__(self, location, K):
+		self.source = location
+		self.K = K
+		self.i = 0
+		self.n = 1
+		self.j = 1
+		self.step = 'simulate'
+		self.simulation = LinesNonUniform(self.source, 1)
+
+	def getLocation(self):
+		if self.simulation is None:
+			return self.source
+		return self.simulation.getLocation()
 
 	def act(self):
-		return True
+		if self.step == 'advance':
+			self.advace()
+		elif self.step == 'simulate':
+			self.simulate()
+
+	def advance(self):
+		p = self.K + max( self.i - math.floor( math.log(self.n , 2) ) , 0 )
+		max_j = int(2**p)
+		if self.j == max_j:
+			if self.n >= 2**self.i:
+				self.i += 1
+				self.n = 1
+				self.j = 1
+				self.simulation = LinesNonUniform(self.source, 2**self.i)
+			else:
+				self.n *= 2
+				self.j = 1
+		else:
+			self.j += 1
+
+		self.step = 'simulate'
+		self.simulate()
+
+
+	def simulate(self):
+		if self.simulation.step == 'origin':
+			self.simulation.act()
+			self.step = 'advance'
+		else:
+			self.simulation.act()
+
+
